5105773e73063547246099d013762f4611b3db01
MD Arif Uddin <78405318+mdarifuddin1@users.noreply.github.com>
2021-04-02 11:17:47 +0600

    Add files via upload
    pattern-2 use C program

---
+++ pattern-2.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main (){
+
+      int num ,row,col;
+
+      printf("Enter a  number");
+      scanf("%d",&num);
+
+
+      for(row=num; row>=1; row-- ){
+
+        for(col=1; col<=row; col++){
+
+            printf("%d",col);
+        }
+
+        printf("\n");
+      }
+
+return 0;
+}
